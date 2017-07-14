022b77e77369f91aa3d74626d735c42c2c1e52fb
Simon Torres <storres@ctio.noao.edu>
2017-07-14 18:35:36 -0400

    agregue ejemplo

--- periodic/core.py
+++ periodic/core.py
@@ -1,2 +1,11 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
+from ejemplo import MiClase
+from ejemplo.ejemplo import mi_funcion
+# from ejemplo.ejemplo import MiClase
+
+clas = MiClase()
+
+clas.saludar()
+mi_funcion('h', 1.4, True)
